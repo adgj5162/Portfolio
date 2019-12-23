9ef25d1ad1f4a41e9d24a3799ec87054dcecec3f
adgj5162 <adgj5162@naver.com>
2019-12-23 14:51:39 +0900

    middle commit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,50 @@
+// navbar click scroll
 $(document).ready(function () {
+
     $('.navbar a, .detail a').click(function (e) { 
         e.preventDefault();
         $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
     });
+});
+
+// header navbar scroll
+$(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+          $('nav').addClass('black');
+    }
+
+    else {
+          $('nav').removeClass('black');
+    }
+})
+
+$(document).ready(function() {
+    $(".menu-icon").on("click", function() {
+          $("nav ul").toggleClass("showing");
+    });
+});
+
+// scroll fade in
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hide').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},100);
+                    
+            }
+            
+        }); 
+    
+    });
+    
 });
